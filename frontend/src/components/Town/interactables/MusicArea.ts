2c6a6c8b9c35724ce63b203070b79e281d281416
Joshua Lin <lin.jos@northeastern.edu>
2023-11-14 17:10:43 -0500

    TEXT SHOWS UP WHEN WALKING OVER, STILL NEED WORK ON MUSICAREA VIEW

--- frontend/src/components/Town/interactables/MusicArea.ts
+++ frontend/src/components/Town/interactables/MusicArea.ts
@@ -1,8 +1,36 @@
 import Interactable, { KnownInteractableTypes } from '../Interactable';
 import { BoundingBox } from '../../../types/CoveyTownSocket';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useInteractable, useInteractableAreaController } from '../../../classes/TownController';
+import useTownController from '../../../hooks/useTownController';
+import MusicAreaController from '../../../classes/interactable/MusicAreaController';
+
 
 export default class MusicArea extends Interactable {
   private _isInteracting = false;
+
+  private _labelText?: Phaser.GameObjects.Text;
+  // const musicAreaController = useInteractableAreaController<MusicAreaController>();
+  ///AHDFJLASFJNKASF;JDN;;NFSN;NDDSNFDN;
+  // TODO
+
+
+  // const townController = useTownController();
+  // useEffect(() => {
+  //   const updateGameState = () => {
+  //     // set queue status
+  //     // set play or pause status
+  //     // set which song?
+  //   };
+
+  //   musicAreaController.addListener('queueUpdated', onGameEnd);
+  //   return () => {
+  //     musicAreaController.removeListener('queueUpdated', updateGameState);
+  //   };
+  // }, [townController, musicAreaController]);
+
+
+  // below regards satisfying the Interactable interface
 
   addedToScene() {
     super.addedToScene();
@@ -15,6 +43,13 @@ export default class MusicArea extends Interactable {
       this.name,
       { color: '#FFFFFF', backgroundColor: '#000000' },
     );
+    this._labelText = this.scene.add.text(
+      this.x - this.displayWidth / 2,
+      this.y - this.displayHeight / 2,
+      `Press space to open the jukebox!!!`,
+      { color: '#FFFFFF', backgroundColor: '#000000' },
+    ).setDepth(30);
+    this._labelText.setVisible(false);
   }
 
 
@@ -28,12 +63,24 @@ export default class MusicArea extends Interactable {
       this.townController.interactableEmitter.emit('endInteraction', this);
       this._isInteracting = false;
     }
+    this._labelText?.setVisible(false);
   }
 
+  overlap(): void {
+    if (!this._labelText) {
+      throw new Error('Should not be able to overlap with this interactable before added to scene');
+    }
+    const location = this.townController.ourPlayer.location;
+    this._labelText.setX(location.x);
+    this._labelText.setY(location.y);
+    this._labelText.setVisible(true);
+  }
+  
   interact(): void {
+    this._labelText?.setVisible(false);
     this._isInteracting = true;
   }
-
+  
   getType(): KnownInteractableTypes {
     return 'musicArea';
   }
