d0dc9d23e5e7f8b2d080641295195f0784a5a4fe
Michael Guo <mowatia@LAPTOP-NDB0NBT7>
2023-11-14 14:11:08 -0500

    added rest of music functions

--- townService/src/town/music/MusicArea.ts
+++ townService/src/town/music/MusicArea.ts
@@ -4,9 +4,11 @@ export default class MusicArea {
 
   message = '';
 
-  currentTrackID: string | null = null;
-
   private _spotify: SpotifyAudioService;
+
+  private _qSong: string[] = [];
+
+  private _currentSongIndex = 0;
 
   constructor(spotify: SpotifyAudioService) {
     this._spotify = spotify;
@@ -21,17 +23,26 @@ export default class MusicArea {
     }
   }
 
-  search(songName: string): void {
+  leave(): void {}
+
+  search(songName: string): string {
     try {
+      // how do we save/return this result?
+      // will this give us an ID or what?
       this._spotify.searchSongs(songName);
     } catch (error) {
       this.message = `${(error as Error).message}`;
     }
   }
 
+  private _getCurrentTrackID(): string | null {
+    return this._qSong[this._currentSongIndex];
+  }
+
   play(): void {
-    if (this.currentTrackID) {
-      this._spotify.playTrack(this.currentTrackID);
+    const songToPlay = this._getCurrentTrackID();
+    if (songToPlay != null) {
+      this._spotify.playTrack(songToPlay);
       this.isPLaying = true;
     } else {
       this.message = 'No current track loaded';
@@ -42,6 +53,24 @@ export default class MusicArea {
   pause(): void {
     this._spotify.pause().then(() => {
       this.isPLaying = false;
+      this._spotify.pause();
     });
+  }
+
+  skip(): void {
+    this._currentSongIndex++;
+  }
+
+  back(): void {
+    if (this._currentSongIndex > 0) {
+      this._currentSongIndex--;
+    } else {
+      this.message = 'Cannot go back';
+    }
+  }
+
+  addToQueue(songName: string): void {
+    this._qSong.push(this.search(songName));
+    this._currentSongIndex++;
   }
 }
